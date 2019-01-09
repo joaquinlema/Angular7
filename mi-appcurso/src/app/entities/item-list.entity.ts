8a0c4232cd1593494e1ad2d3a0e4189cbf736f4f
joaquin lema <joaco.lema@hotmail.com>
2019-01-09 16:22:29 -0300

    agregamos ; que faltaba

--- mi-appcurso/src/app/entities/item-list.entity.ts
+++ mi-appcurso/src/app/entities/item-list.entity.ts
@@ -28,9 +28,9 @@ export class PerfilesList extends GenericList {
   constructor() {
     super();
     this.list = [
-      new ItemList(0 , 'Desarrollador Junior'),
+      new ItemList(0 , 'Desarrollador Junior '),
       new ItemList(1 , 'Desarrollador Semi'),
       new ItemList(2 , 'Desarrollador Senior'),
-    ]
+    ];
   }
 }
