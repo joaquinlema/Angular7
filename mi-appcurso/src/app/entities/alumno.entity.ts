18d03656adf0acd6d20c7a3b01f36d6bed79aebc
joaquin lema <joaco.lema@hotmail.com>
2019-01-09 16:13:46 -0300

    commit desde terminal

---
+++ mi-appcurso/src/app/entities/alumno.entity.ts
@@ -0,0 +1,12 @@
+export class Alumno {
+  constructor(
+  public id: number,
+  public nombre: string,
+  public apellido: string,
+  public sexoID: number,
+  public activo: boolean,
+  public perfilID: number
+  ) {}
+  }
+  const alumnoData = new Alumno(1, 'Juan', 'Perez', 1, true, 0);
+  export { alumnoData };
