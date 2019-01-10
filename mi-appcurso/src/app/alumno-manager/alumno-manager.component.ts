f533f8207ef54569b9a1992e883a6b5fc754f384
joaquin lema <joaco.lema@hotmail.com>
2019-01-10 13:42:26 -0300

    agregamos alumnos y alumnos data en Amanager

--- mi-appcurso/src/app/alumno-manager/alumno-manager.component.ts
+++ mi-appcurso/src/app/alumno-manager/alumno-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Alumno, AlumnoData } from '../entities/alumno.entity';
 
 @Component({
   selector: 'app-alumno-manager',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alumno-manager.component.css']
 })
 export class AlumnoManagerComponent implements OnInit {
+  private _alumnosData: AlumnoData;
+  alumnoSeleccionado: Alumno = null;
+  alumnos: Alumno[];
 
-  constructor() { }
+  constructor() {
+    this._alumnosData  = new AlumnoData();
+    this.alumnos = this._alumnosData.GetAll();
+  }
+
 
   ngOnInit() {
   }
